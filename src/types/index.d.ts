f695bff128a453a066c6ad538d2d29080493dbc7
Fasel Godbcho <faselgodbcho@gmail.com>
2025-06-29 13:48:23 +0300

    Update index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,14 @@
-type AuthMode = 'login' | 'register'
+type AuthMode = "login" | "register";
+
+type Session = {
+  id: number;
+  sender: "user" | "ai";
+  content: string;
+  pdf: {
+    title: string;
+  };
+};
+
+type GetUserSessionsResult =
+  | { data: Session[]; error: null }
+  | { data: null; error: string };
